8e2989bf5ac74c985dc3b9e248ae5ded0e6d6f47
Nathan Parkinson <ntp2k@yahoo.co.uk>
2018-11-30 19:40:56 +0000

    merged Includable and IncludableQueryable

--- Include/IIncludableQueryable.cs
+++ Include/IIncludableQueryable.cs
@@ -5,10 +5,10 @@ using System.Linq.Expressions;
 
 namespace LinqToDB.Utils
 {
-    public interface IIncludableQueryable<TClass> where TClass : class
-    {
-        IQueryable<TClass> Query { get; }
-        IIncludableQueryable<TClass> AddExpression<TProperty>(Expression<Func<TClass, TProperty>> expr) where TProperty : class;
-        List<TClass> LoadEntityMap(IIncludableQueryable<TClass> includable);
-    }
+    //public interface IIncludableQueryable<TClass> where TClass : class
+    //{
+    //    IQueryable<TClass> Query { get; }
+    //    IIncludableQueryable<TClass> AddExpression<TProperty>(Expression<Func<TClass, TProperty>> expr) where TProperty : class;
+    //    List<TClass> LoadEntityMap(IIncludableQueryable<TClass> includable);
+    //}
 }
--- Include/IncludeExtensions.cs
+++ Include/IncludeExtensions.cs
@@ -1,44 +1,206 @@
-﻿using LinqToDB.Mapping;
+﻿using LinqToDB.Async;
+using LinqToDB.Linq;
+using LinqToDB.Mapping;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LinqToDB.Utils
 {
-    //HOW TO DO ThenInclude now :-/
-    internal class IncludableQueryable<TClass> : IIncludableQueryable<TClass> where TClass : class
-    {
-        private readonly IRootAccessor<TClass> _rootAccessor;
 
-        internal IncludableQueryable(IQueryable<TClass> query)
+    public interface IIncludableQueryable<TClass> : IExpressionQuery<TClass> where TClass : class
+    {
+        IIncludableQueryable<TClass> AddExpression<TProperty>(Expression<Func<TClass, TProperty>> expr) 
+            where TProperty : class;
+    }
+
+    public class IncludableQueryable<T> : IIncludableQueryable<T> where T : class
+    {
+        private readonly IRootAccessor<T> _rootAccessor;
+
+        internal IncludableQueryable(IQueryable<T> query)
         {
-            var context = query.GetDataContext<IDataContext>();            
-            _rootAccessor = new RootAccessor<TClass>(context.MappingSchema);
-            Query = query;
+            LinqToDBQuery = query as IExpressionQuery<T>;
+            if (LinqToDBQuery == null)
+            {
+                throw new ArgumentException("IQueryable<T> must be of type LinqToDB.Linq.IExpressionQuery<T>");
+            }
+
+            var context = query.GetDataContext<IDataContext>();
+            _rootAccessor = new RootAccessor<T>(context.MappingSchema);
         }
 
-        public IQueryable<TClass> Query { get; }
+        internal IncludableQueryable(IQueryable<T> query, IRootAccessor<T> rootAccessor)
+        {
+            LinqToDBQuery = query as IExpressionQuery<T>;
+            if (LinqToDBQuery == null)
+            {
+                throw new ArgumentException("IQueryable<T> must be of type LinqToDB.Linq.IExpressionQuery<T>");
+            }
 
-        public IIncludableQueryable<TClass> AddExpression<TProperty>(Expression<Func<TClass, TProperty>> expr)
+            var context = query.GetDataContext<IDataContext>();
+            _rootAccessor = rootAccessor;
+        }
+
+        public IIncludableQueryable<T> AddExpression<TProperty>(Expression<Func<T, TProperty>> expr)
             where TProperty : class
         {
-            var visitor = new PropertyVisitor<TClass>(_rootAccessor);
+            var visitor = new PropertyVisitor<T>(_rootAccessor);
             visitor.MapProperties(expr);
             return this;
         }
-        
-        public List<TClass> LoadEntityMap(IIncludableQueryable<TClass> includable)
-        {
-            var query = includable.Query;
-            var entities = query.ToList();
-            _rootAccessor.LoadMap(entities, query);
 
-            return entities;
+        public IExpressionQuery<T> LinqToDBQuery { get; }
+
+        public string SqlText => LinqToDBQuery.SqlText;
+
+        IAsyncEnumerable<TResult> IQueryProviderAsync.ExecuteAsync<TResult>(Expression expression)
+            => LinqToDBQuery.ExecuteAsync<TResult>(expression);
+
+        async Task<TResult> IQueryProviderAsync.ExecuteAsync<TResult>(Expression expression, CancellationToken token)
+            => await LinqToDBQuery.ExecuteAsync<TResult>(expression, token);
+
+
+        public Type ElementType => LinqToDBQuery.ElementType;
+
+        public Expression Expression => LinqToDBQuery.Expression;
+
+        public IQueryProvider Provider => this;// LinqToDBQuery.Provider;
+
+
+        public IDataContext DataContext => LinqToDBQuery.DataContext;
+
+        Expression IExpressionQuery<T>.Expression
+        {
+            get => LinqToDBQuery.Expression;
+            set => LinqToDBQuery.Expression = value;
+        }
+
+
+        IQueryProvider IQueryable.Provider => this;// LinqToDBQuery.Provider;
+        Expression IQueryable.Expression => LinqToDBQuery.Expression;
+        Type IQueryable.ElementType => LinqToDBQuery.ElementType;
+
+        IQueryable IQueryProvider.CreateQuery(Expression expression)
+        {
+            var queryable = LinqToDBQuery.CreateQuery(expression) as IExpressionQuery<T>;
+            return new IncludableQueryable<T>(queryable);
+        }
+
+        IQueryable<TElement> IQueryProvider.CreateQuery<TElement>(Expression expression)
+        {
+            if (!typeof(TElement).IsClass)
+            {
+                //TODO Test this works
+                return LinqToDB.Linq.Internals.CreateExpressionQueryInstance<TElement>(DataContext, Expression);
+                //throw new ArgumentException("TElement must be a class");
+            }
+
+            var d1 = typeof(IncludableQueryable<>);
+            Type[] typeArgs = { typeof(TElement) };
+            var makeme = d1.MakeGenericType(typeArgs);
+
+            var flags = BindingFlags.NonPublic | BindingFlags.Instance;
+            CultureInfo culture = null; // use InvariantCulture or other if you prefer
+            //object instantiatedType =
+            //  Activator.CreateInstance(typeToInstantiate, flags, null, parameter, culture);
+
+            var parameters = new object[0];
+            if (typeof(TElement) == typeof(T))
+            {
+                parameters = new object[]
+                {
+                    LinqToDBQuery.CreateQuery<TElement>(expression),
+                    _rootAccessor
+                };
+            }
+            else
+            {
+                parameters = new object[]
+                {
+                    LinqToDBQuery.CreateQuery<TElement>(expression)
+                };
+            }
+
+
+
+            var returnQueryable = Activator.CreateInstance(makeme, flags, null, parameters, culture) 
+                as IQueryable<TElement>;
+
+            return returnQueryable;
+        }
+
+
+        object IQueryProvider.Execute(Expression expression) => LinqToDBQuery.Execute(expression);
+
+        TResult IQueryProvider.Execute<TResult>(Expression expression)
+            => LinqToDBQuery.Execute<TResult>(expression);
+
+
+        //TODO change the 2 methods below to convert to list and then add all the child items
+        //then return  list.GetEnumerator();
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            var entities = LinqToDBQuery.ToList();
+
+            _rootAccessor.LoadMap(entities, this);
+
+            return entities.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            var entities = LinqToDBQuery.ToList();
+
+            _rootAccessor.LoadMap(entities, this);
+
+            return ((IEnumerable)entities).GetEnumerator();
         }
     }
+
+
+
+
+
+
+    ////HOW TO DO ThenInclude now :-/
+    //internal class IncludableQueryableOld<TClass> : IIncludableQueryable<TClass> where TClass : class
+    //{
+    //    private readonly IRootAccessor<TClass> _rootAccessor;
+
+    //    internal IncludableQueryableOld(IQueryable<TClass> query)
+    //    {
+    //        var context = query.GetDataContext<IDataContext>();            
+    //        _rootAccessor = new RootAccessor<TClass>(context.MappingSchema);
+    //        Query = query;
+    //    }
+
+    //    public IQueryable<TClass> Query { get; }
+
+    //    public IIncludableQueryable<TClass> AddExpression<TProperty>(Expression<Func<TClass, TProperty>> expr)
+    //        where TProperty : class
+    //    {
+    //        var visitor = new PropertyVisitor<TClass>(_rootAccessor);
+    //        visitor.MapProperties(expr);
+    //        return this;
+    //    }
+
+    //    public List<TClass> LoadEntityMap(IIncludableQueryable<TClass> includable)
+    //    {
+    //        var query = includable.Query;
+    //        var entities = query.ToList();
+    //        _rootAccessor.LoadMap(entities, query);
+
+    //        return entities;
+    //    }
+    //}
 
 
 
@@ -54,6 +216,7 @@ namespace LinqToDB.Utils
             where TProperty : class
             => includable.AddExpression(expr);
 
+        /*
         public static List<TClass> ToList<TClass>(this IIncludableQueryable<TClass> includable)
             where TClass : class
         {
@@ -66,5 +229,6 @@ namespace LinqToDB.Utils
             var entities = includeImpl.LoadEntityMap(includable);            
             return entities;
         }
+        */
     }
 }
