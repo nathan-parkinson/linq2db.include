9de40a8593196afce01f1e64a4e633d0ecb43433
Nathan Parkinson <ntp2k@yahoo.co.uk>
2018-12-02 15:44:56 +0000

    moved code to own file

--- Include/IIncludableQueryable.cs
+++ Include/IIncludableQueryable.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using LinqToDB.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 
 namespace LinqToDB.Utils
 {
-    //public interface IIncludableQueryable<TClass> where TClass : class
-    //{
-    //    IQueryable<TClass> Query { get; }
-    //    IIncludableQueryable<TClass> AddExpression<TProperty>(Expression<Func<TClass, TProperty>> expr) where TProperty : class;
-    //    List<TClass> LoadEntityMap(IIncludableQueryable<TClass> includable);
-    //}
+    public interface IIncludableQueryable<TClass> : IExpressionQuery<TClass> where TClass : class
+    {
+        IIncludableQueryable<TClass> AddExpression<TProperty>(Expression<Func<TClass, TProperty>> expr)
+            where TProperty : class;
+    }
 }
