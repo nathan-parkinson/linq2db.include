ffc9f9b3ad4cebd7d7b95106e61e0cea5c429183
Nathan Parkinson <nathan.net83@gmail.com>
2019-02-03 18:36:12 +0000

    Add Queryable Execute\GetEnumerator tests

--- src/LinqToDB.Include.Tests/LinqToDB.Include.Tests/ExecuteTests.cs
+++ src/LinqToDB.Include.Tests/LinqToDB.Include.Tests/ExecuteTests.cs
@@ -11,16 +11,12 @@ using App.Domain.Repository.Mapping;
 
 namespace Tests
 {
-    public class Tests
+    public class ExecuteTests
     {
         [SetUp]
         public void Setup()
         {
-            if (DataConnection.DefaultSettings == null)
-            {
-                DataConnection.DefaultSettings = new DBConnection();
-                SQLitePCL.Batteries.Init();
-            }
+            DataConnection.DefaultSettings = DataConnection.DefaultSettings ?? new DBConnection();
         }
 
         [Test]
@@ -36,7 +32,7 @@ namespace Tests
 
                 query = query.Include(x => x.Spouse);
                 var p = query.First();
-                
+
                 Assert.IsNotNull(p.Spouse);
             }
         }
@@ -94,6 +90,26 @@ namespace Tests
         }
 
 
+        [Test]
+        public void TestCallingExecuteDoesNotChangeQuery()
+        {
+            using (var db = new DBContext())
+            {
+                AddData(db);
+
+                var query = from x in db.People
+                            select x;
+
+                query = query.Include(x => x.Spouse);
+                var p1 = query.ElementAt(1);
+
+                var p = query.FirstOrDefault(x => x.FirstName == "Jim");
+
+                Assert.AreEqual(p1.PersonId, p.PersonId);
+                Assert.Greater(query.ToList().Count, 1);
+                Assert.IsNotNull(p.Spouse);
+            }
+        }
 
 
         [Test]
@@ -218,7 +234,7 @@ namespace Tests
                 LastName = "Jones",
                 Dob = new DateTime(2011, 11, 10)
             };
-            
+
             person.PersonId = db.InsertWithInt32Identity(person);
 
             var personOther = new Person
@@ -233,56 +249,58 @@ namespace Tests
 
 
         }
-    }
 
-    public class DBContext : DataConnection
-    {
-        public DBContext() : base("DBConn")
+
+        public class DBContext : DataConnection
         {
-            var builder = MappingSchema.Default.GetFluentMappingBuilder();
+            public DBContext() : base("DBConn")
+            {
 
-            builder.Entity<Person>()
-                .Association(x => x.Orders, (p, o) => p.PersonId == o.PersonId)
-                .Property(x => x.PersonId).IsIdentity().IsPrimaryKey().IsNullable(false)
-                .Property(x => x.FirstName).HasLength(100).IsNullable(false)
-                .Property(x => x.LastName).HasLength(100).IsNullable(false)
-                .Property(x => x.Dob).IsNullable(false)
-                .Property(x => x.Salary).IsNullable(false)
-                .Property(x => x.Weight).IsNullable(false)
-                .Property(x => x.SpouseId).IsNullable()
-                .Property(x => x.Spouse).IsNotColumn().Association(x => x.Spouse, p => p.SpouseId, s => s.PersonId)
-                .Property(x => x.Orders).IsNotColumn();
+                var builder = MappingSchema.Default.GetFluentMappingBuilder();
 
-            builder.Entity<Order>()
-                .Association(x => x.ProductLines, (o, p) => o.OrderId == p.OrderId, true)
-                .Property(x => x.OrderId).IsIdentity().IsPrimaryKey().IsNullable(false)
-                .Property(x => x.OrderNumber).HasLength(100).IsNullable(false)
-                .Property(x => x.OrderedOn).IsNullable(false).HasSkipOnUpdate()
-                .Property(x => x.PersonId).IsNullable(false)
-                .Property(x => x.Person).IsNotColumn()
-                .HasAttribute(new AssociationAttribute { ThisKey = nameof(Order.PersonId), OtherKey = nameof(Person.PersonId), CanBeNull = false });
+                builder.Entity<Person>()
+                    .Association(x => x.Orders, (p, o) => p.PersonId == o.PersonId)
+                    .Property(x => x.PersonId).IsIdentity().IsPrimaryKey().IsNullable(false)
+                    .Property(x => x.FirstName).HasLength(100).IsNullable(false)
+                    .Property(x => x.LastName).HasLength(100).IsNullable(false)
+                    .Property(x => x.Dob).IsNullable(false)
+                    .Property(x => x.Salary).IsNullable(false)
+                    .Property(x => x.Weight).IsNullable(false)
+                    .Property(x => x.SpouseId).IsNullable()
+                    .Property(x => x.Spouse).IsNotColumn().Association(x => x.Spouse, p => p.SpouseId, s => s.PersonId)
+                    .Property(x => x.Orders).IsNotColumn();
 
-            builder.Entity<ProductLine>()
-                .Inheritance(x => x.ProductLineType, ProductLineType.Normal, typeof(ProductLine), true)
-                .Inheritance(x => x.ProductLineType, ProductLineType.Extended, typeof(ExtendedProductLine))
-                .Property(x => x.ProductLineId).IsIdentity().IsPrimaryKey().IsNullable(false)
-                .Property(x => x.OrderId).IsNullable(false)
-                .Property(x => x.ProductLineType).IsNullable(false).IsDiscriminator()
-                .Property(x => x.ProductId).IsNullable(false)
-                .Property(x => x.ProductCode).HasLength(20).IsNullable(false)
-                .Association(x => x.Order, p => p.OrderId, o => o.OrderId);
+                builder.Entity<Order>()
+                    .Association(x => x.ProductLines, (o, p) => o.OrderId == p.OrderId, true)
+                    .Property(x => x.OrderId).IsIdentity().IsPrimaryKey().IsNullable(false)
+                    .Property(x => x.OrderNumber).HasLength(100).IsNullable(false)
+                    .Property(x => x.OrderedOn).IsNullable(false).HasSkipOnUpdate()
+                    .Property(x => x.PersonId).IsNullable(false)
+                    .Property(x => x.Person).IsNotColumn()
+                    .HasAttribute(new AssociationAttribute { ThisKey = nameof(Order.PersonId), OtherKey = nameof(Person.PersonId), CanBeNull = false });
 
-            builder.Entity<ExtendedProductLine>()
-                .HasAttribute(new TableAttribute { Name = nameof(ProductLine), IsColumnAttributeRequired = true })
-                .Association(x => x.FirstPerson, (pl, p) => p.PersonId == 1);
+                builder.Entity<ProductLine>()
+                    .Inheritance(x => x.ProductLineType, ProductLineType.Normal, typeof(ProductLine), true)
+                    .Inheritance(x => x.ProductLineType, ProductLineType.Extended, typeof(ExtendedProductLine))
+                    .Property(x => x.ProductLineId).IsIdentity().IsPrimaryKey().IsNullable(false)
+                    .Property(x => x.OrderId).IsNullable(false)
+                    .Property(x => x.ProductLineType).IsNullable(false).IsDiscriminator()
+                    .Property(x => x.ProductId).IsNullable(false)
+                    .Property(x => x.ProductCode).HasLength(20).IsNullable(false)
+                    .Association(x => x.Order, p => p.OrderId, o => o.OrderId);
 
-            this.CreateTable<Person>();
-            this.CreateTable<Order>();
-            this.CreateTable<ProductLine>();
+                builder.Entity<ExtendedProductLine>()
+                    .HasAttribute(new TableAttribute { Name = nameof(ProductLine), IsColumnAttributeRequired = true })
+                    .Association(x => x.FirstPerson, (pl, p) => p.PersonId == 1);
+
+                this.CreateTable<Person>();
+                this.CreateTable<Order>();
+                this.CreateTable<ProductLine>();
+            }
+
+            public ITable<Person> People => GetTable<Person>();
+            public ITable<Order> Orders => GetTable<Order>();
+            public ITable<ProductLine> ProductLines => GetTable<ProductLine>();
         }
-
-        public ITable<Person> People => GetTable<Person>();
-        public ITable<Order> Orders => GetTable<Order>();
-        public ITable<ProductLine> ProductLines => GetTable<ProductLine>();        
     }
 }
